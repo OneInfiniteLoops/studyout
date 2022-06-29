9b2201532e5172953a1c8dce837199c5f03c6cac
caolanhamilton <caolanhamilton75@gmail.com>
2022-06-29 09:10:21 +0100

    Fix iPhone notch with SafeArea for all views

--- Pages/Map.js
+++ Pages/Map.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Map() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Maps</Text>
-    </View>
+    </SafeAreaView>
   );
 }
