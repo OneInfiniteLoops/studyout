5aedd9316437fc914f1bb0f5a842b4c0e9307031
caolanhamilton <caolanhamilton75@gmail.com>
2022-07-04 11:19:20 +0100

    Create place view with props from stack

---
+++ Pages/SecondaryPages/PlaceView.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function PlaceView(location) {
+  console.log(location)
+  return (
+    <View>
+      <Text>PlaceView</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
