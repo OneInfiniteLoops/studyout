a4e12db9085fbeb10d8ec8b99cdb5e703428c9fd
garysum88 <102538356+garysum88@users.noreply.github.com>
2022-07-04 11:57:11 +0100

    Update Account page and AccountStack setup

--- Stacks/AccountStack.js
+++ Stacks/AccountStack.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Account from '../Pages/Account';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Register from '../Pages/SecondaryPages/Register';
 
 export default function ListViewStack() {
        const Stack = createNativeStackNavigator();
@@ -11,6 +12,11 @@ export default function ListViewStack() {
              name="Account page"
              component={Account}
              options={{ headerShown: false }}
+           ></Stack.Screen>
+           <Stack.Screen
+             name="Register"
+             component={Register}
+             options={{ headerShown: true }}
            ></Stack.Screen>
          </Stack.Navigator>
        );
