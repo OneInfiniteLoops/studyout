1b3792daf2031678342a4462e6bd0acbde23e7b8
caolanhamilton <caolanhamilton75@gmail.com>
2022-07-04 10:30:56 +0100

    Create place view subpage

---
+++ Pages/SecondaryPages./PlaceView.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function PlaceView() {
+  return (
+    <View>
+      <Text>PlaceView</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
