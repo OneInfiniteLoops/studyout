287bae3583d43f3dd7bc1617f1866776ad1dd0ef
jaykeHarrison <jayke@jaykeharrison.co.uk>
2022-07-05 23:08:05 +0100

    add location template object

---
+++ Utils/locationObj.js
@@ -0,0 +1,11 @@
+export const LocationObj = {
+  LocationID: null,
+  LocationName: null,
+  Address: null,
+  Postcode: null,
+  Longitude: null,
+  Latitude: null,
+  Condition: null,
+  ImgUrl: null,
+  CreatedBy: null,
+};
