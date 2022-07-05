cea73750df40a62e94faeaa88dad68039d0900f5
1-Infiniteloop <skjyeung@gmail.com>
2022-07-05 14:56:06 +0100

    add util function to GET locations data from backend server

--- Utils/api.js
+++ Utils/api.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3000/api',
-})
+  baseURL: "http://127.0.0.1:3000/api",
+});
 
 export const createUser = (username, firstName, lastName, email) => {
+  const userObj = {
+    Username: username,
+    FirstName: firstName,
+    LastName: lastName,
+    Email: email,
+  };
+  return api.post(`/users`, userObj);
+};
 
-    const userObj = { 
-        "Username": username,
-        "FirstName": firstName,
-        "LastName": lastName,
-        "Email": email
-    }
-    return api.post(`/users`,userObj)
-  
-  }
+export const getUserById = (userId) => {
+  return api.get(`/users/${userId}`);
+};
 
-
-  export const getUserById = (userId) => {
-
-    return api.get(`/users/${userId}`)
-  
-  }
+export const getLocations = () => {
+  return api.get(`/locations`).then((res) => {
+    return res.data;
+  });
+};
