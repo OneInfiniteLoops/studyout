3ada157ef6292ba49df03cad5be7330c05e7c4de
1-Infiniteloop <skjyeung@gmail.com>
2022-07-06 17:03:06 +0100

    Import Ionicons, and add styling to tabBarIcon and tabBarLabel

--- App.js
+++ App.js
@@ -11,49 +11,88 @@ import MapStack from "./Stacks/MapStack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GOOGLE_MAPS_KEY } from "@env";
 import { useState } from "react";
-import { UserLoginContext } from "./Contexts/user"
-
-
+import { UserLoginContext } from "./Contexts/user";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-
-  const [userLogin, setUserLogin]  = useState({
-    "ID": 1,
-    "Username": "garyg",
-    "FirstName": "Gary",
-    "LastName": "Summmmm",
-    "Email": "gary@gmail.com"
-  })
-
+  const [userLogin, setUserLogin] = useState({
+    ID: 1,
+    Username: "garyg",
+    FirstName: "Gary",
+    LastName: "Summmmm",
+    Email: "gary@gmail.com",
+  });
 
   return (
-    <UserLoginContext.Provider value={{userLogin, setUserLogin}}>
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={ListViewStack}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Map"
-          component={MapStack}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Bookmarks"
-          component={BookmarksStack}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Account"
-          component={AccountStack}
-          options={{ headerShown: false }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-   </UserLoginContext.Provider>
+    <UserLoginContext.Provider value={{ userLogin, setUserLogin }}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              let colour;
+
+              if (route.name === "Home") {
+                iconName = focused ? "list" : "list-outline";
+                colour = focused ? "#ff385c" : "#484848";
+              } else if (route.name === "Map") {
+                iconName = focused ? "map" : "map-outline";
+                colour = focused ? "#ff385c" : "#484848";
+              } else if (route.name === "Account") {
+                iconName = focused ? "person" : "person-outline";
+                colour = focused ? "#ff385c" : "#484848";
+              } else if (route.name === "Bookmarks") {
+                iconName = focused ? "star" : "star-outline";
+                colour = focused ? "#ff385c" : "#484848";
+              }
+              return <Ionicons name={iconName} size={size} color={colour} />;
+            },
+            tabBarLabel: ({ focused, color }) => {
+              let label;
+              let colour;
+
+              if (route.name === "Home") {
+                label = "List";
+                colour = focused ? "#ff385c" : "#222222";
+              } else if (route.name === "Map") {
+                label = "Map";
+                colour = focused ? "#ff385c" : "#222222";
+              } else if (route.name === "Account") {
+                label = "Account";
+                colour = focused ? "#ff385c" : "#222222";
+              } else if (route.name === "Bookmarks") {
+                label = "Bookmarks";
+                colour = focused ? "#ff385c" : "#222222";
+              }
+
+              return <Text style={{ color: colour }}>{label}</Text>;
+            },
+          })}
+        >
+          <Tab.Screen
+            name="Home"
+            component={ListViewStack}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Map"
+            component={MapStack}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Bookmarks"
+            component={BookmarksStack}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Account"
+            component={AccountStack}
+            options={{ headerShown: false }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </UserLoginContext.Provider>
   );
 }
