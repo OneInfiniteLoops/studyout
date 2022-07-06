0d875c0d68da3a4d54f7ff2725f168c5886ee592
1-Infiniteloop <skjyeung@gmail.com>
2022-07-06 11:25:27 +0100

    remove mock data from listView

--- Pages/ListView.js
+++ Pages/ListView.js
@@ -19,63 +19,6 @@ import { getLocationDistance } from "../Utils/getLocationDistance";
 import * as Location from "expo-location";
 import { getLocations } from "../Utils/api";
 
-const locations = [
-  {
-    location_id: 1,
-    location_name: "Costa Market Street",
-    location_address: "28 Market Street, Manchester M1 1PW England",
-    postcode: "M11PW",
-    latitude: "53.48376591153698",
-    longitude: "-2.242955959297516",
-    features: {
-      wifi: true,
-      food: true,
-      parking: true,
-    },
-    conditions: "Buy one coffee",
-    opening_hours: "9:00am - 5:00pm",
-    image_url:
-      "https://media-cdn.tripadvisor.com/media/photo-s/15/0a/f0/52/20181013-164708-largejpg.jpg",
-    created_by: "GarySum123",
-  },
-  {
-    location_id: 2,
-    location_name: "Manchester Central Library",
-    location_address: "St Peter's Square, Manchester M2 5PD",
-    postcode: "M2 5PD",
-    latitude: "53.478030039764135",
-    longitude: "-2.2446160630014624",
-    features: {
-      wifi: true,
-      food: true,
-      parking: false,
-    },
-    conditions: "None",
-    opening_hours: "9:00am - 5:00pm",
-    image_url:
-      "https://eu-assets.simpleview-europe.com/manchester2016/imageresizer/?image=%2Fdmsimgs%2FCentral_Library_publicity_photo.jpg_273973847.png&action=ProductDetail",
-    created_by: "CaolanExplores",
-  },
-  {
-    location_id: 3,
-    location_name: "Longsight Library",
-    location_address: "519 Stockport Rd, Longsight, Manchester M12 4NE",
-    postcode: "M12 4NE",
-    latitude: "53.458445275050494",
-    longitude: "-2.201442223181674",
-    features: {
-      wifi: true,
-      food: true,
-      parking: true,
-    },
-    conditions: "None",
-    opening_hours: "9:00am - 5:00pm",
-    image_url:
-      "https://i0.wp.com/manclibraries.blog/wp-content/uploads/2020/12/longsight-2.jpg?w=1280&ssl=1",
-    created_by: "GoCrazyM8",
-  },
-];
-
 LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
 
 function ListView({ navigation }) {
