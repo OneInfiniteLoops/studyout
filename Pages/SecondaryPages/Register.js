ab0fadcef09f167887478d3d706c4fc1db7902d5
1-Infiniteloop <skjyeung@gmail.com>
2022-07-06 20:24:33 +0100

    Change styling of submit button, title and input fields on registration page

--- Pages/SecondaryPages/Register.js
+++ Pages/SecondaryPages/Register.js
@@ -1,28 +1,45 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Button } from 'react-native';
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { UserLoginContext } from '../../Contexts/user';
-
-import { useForm, Controller } from 'react-hook-form';
-import * as yup from "yup";
-import { yupResolver } from '@hookform/resolvers/yup';
-
 import {
-  createUserWithEmailAndPassword,
-  updateProfile
-} from "firebase/auth";
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Button,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { UserLoginContext } from "../../Contexts/user";
 
-import { authentication } from '../../firebase.config';
-import { createUser, getUserById } from '../../Utils/api';
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+import { authentication } from "../../firebase.config";
+import { createUser, getUserById } from "../../Utils/api";
 
 const schema = yup.object().shape({
-  username: yup.string().required("Username is required").min(5, 'Must be at least 5 characters').max(20, 'Must be no longer than 20 characters'),
-  firstname: yup.string().required("First Name is required").min(3, 'Must be at least 3 characters').max(35, 'Must be no longer than 35 characters'),
-  lastname: yup.string().required("Last Name is required").min(3, 'Must be at least 3 characters').max(35, 'Must be no longer than 35 characters'),
+  username: yup
+    .string()
+    .required("Username is required")
+    .min(5, "Must be at least 5 characters")
+    .max(20, "Must be no longer than 20 characters"),
+  firstname: yup
+    .string()
+    .required("First Name is required")
+    .min(3, "Must be at least 3 characters")
+    .max(35, "Must be no longer than 35 characters"),
+  lastname: yup
+    .string()
+    .required("Last Name is required")
+    .min(3, "Must be at least 3 characters")
+    .max(35, "Must be no longer than 35 characters"),
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup
     .string()
-    .required('Please Enter your password')
+    .required("Please Enter your password")
     .matches(
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
       "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
@@ -30,36 +47,48 @@ const schema = yup.object().shape({
 });
 
 export default function Register({ navigation }) {
-
   const { userLogin, setUserLogin } = React.useContext(UserLoginContext);
-  const { control, handleSubmit, formState: { errors }, reset } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
     resolver: yupResolver(schema),
 
     defaultValues: {
-      'email': '',
-      'password': ''
+      email: "",
+      password: "",
     },
 
-    reValidateMode: 'onSubmit'
+    reValidateMode: "onSubmit",
   });
 
   const submit = (data) => {
     reset();
-    const firebasePromise = createUserWithEmailAndPassword(authentication, data.email, data.password);
-    const createUserPromise = createUser(data.username, data.firstname, data.lastname, data.email);
+    const firebasePromise = createUserWithEmailAndPassword(
+      authentication,
+      data.email,
+      data.password
+    );
+    const createUserPromise = createUser(
+      data.username,
+      data.firstname,
+      data.lastname,
+      data.email
+    );
 
     Promise.all([firebasePromise, createUserPromise])
 
       .then((resolvedArr) => {
         const ourId = resolvedArr[1].data.ID;
         updateProfile(resolvedArr[0].user, { displayName: ourId });
-        return ourId
+        return ourId;
       })
-      .then((ourId)=>{
-        getUserById(ourId)
-        .then((res)=> {
-          setUserLogin(res.data)
-        })
+      .then((ourId) => {
+        getUserById(ourId).then((res) => {
+          setUserLogin(res.data);
+        });
       })
       .catch((err) => {
         alert("An error occured");
@@ -68,23 +97,25 @@ export default function Register({ navigation }) {
 
   if (userLogin) {
     return (
-        <SafeAreaView style={styles.container}>
-          <View style={styles.loggedInContainer}>
-            <Text style={styles.title}>Your account has been created succssfully!</Text>
-            <Text></Text>
-            <Button  
-                    title="Go to my account"  
-                    onPress={() => navigation.navigate("Account page")}  
-                />  
-          </View>
-        </SafeAreaView>
-      );
-    }
+      <SafeAreaView style={styles.container}>
+        <View style={styles.loggedInContainer}>
+          <Text style={styles.title}>
+            Your account has been created succssfully!
+          </Text>
+          <Text></Text>
+          <Button
+            title="Go to my account"
+            onPress={() => navigation.navigate("Account page")}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 
   return (
     <SafeAreaView style={styles.safeareacontainer}>
       <View style={styles.container}>
-        <Text style={styles.title}>Register</Text>
+        <Text style={styles.title}>Account Registration</Text>
 
         <Controller
           control={control}
@@ -93,15 +124,16 @@ export default function Register({ navigation }) {
             <TextInput
               placeholder="Username"
               style={styles.input}
-              autoCapitalize='none'
+              autoCapitalize="none"
               autoCorrect={false}
               onChangeText={(value) => onChange(value)}
               value={value}
             />
           )}
         />
-        <Text style={styles.error}>{errors.username ? errors.username.message : null}</Text>
-
+        <Text style={styles.error}>
+          {errors.username ? errors.username.message : null}
+        </Text>
 
         <Controller
           control={control}
@@ -115,7 +147,9 @@ export default function Register({ navigation }) {
             />
           )}
         />
-        <Text style={styles.error}>{errors.firstname ? errors.firstname.message : null}</Text>
+        <Text style={styles.error}>
+          {errors.firstname ? errors.firstname.message : null}
+        </Text>
 
         <Controller
           control={control}
@@ -129,7 +163,9 @@ export default function Register({ navigation }) {
             />
           )}
         />
-        <Text style={styles.error}>{errors.lastname ? errors.lastname.message : null}</Text>
+        <Text style={styles.error}>
+          {errors.lastname ? errors.lastname.message : null}
+        </Text>
 
         <Controller
           control={control}
@@ -138,17 +174,19 @@ export default function Register({ navigation }) {
             <TextInput
               placeholder="Email"
               style={styles.input}
-              textContentType='emailAddress'
-              keyboardType='email-address'
-              autoCapitalize='none'
+              textContentType="emailAddress"
+              keyboardType="email-address"
+              autoCapitalize="none"
               autoCorrect={false}
-              autoCompleteType='email'
+              autoCompleteType="email"
               onChangeText={(value) => onChange(value)}
               value={value}
             />
           )}
         />
-        <Text style={styles.error}>{errors.email ? errors.email.message : null}</Text>
+        <Text style={styles.error}>
+          {errors.email ? errors.email.message : null}
+        </Text>
         <Controller
           control={control}
           name="password"
@@ -162,75 +200,76 @@ export default function Register({ navigation }) {
             />
           )}
         />
-        <Text style={styles.error}>{errors.password ? errors.password.message : null}</Text>
+        <Text style={styles.error}>
+          {errors.password ? errors.password.message : null}
+        </Text>
 
-        <TouchableOpacity
-          onPress={handleSubmit(submit)}>
-          <Text style={styles.button}>Submit</Text>
+        <TouchableOpacity style={styles.button} onPress={handleSubmit(submit)}>
+          <Text style={styles.buttonText}>Submit</Text>
         </TouchableOpacity>
-
       </View>
     </SafeAreaView>
   );
 }
-  
-
 
 const styles = StyleSheet.create({
   safeareacontainer: {
-    flex: 1
+    flex: 1,
   },
   container: {
     flex: 1,
     // backgroundColor: '#282828',
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   title: {
     fontSize: 25,
-    marginBottom: 30,
-    marginTop: 16,
+    marginBottom: 50,
   },
   error: {
     fontSize: 16,
-    color: 'red',
+    color: "red",
     marginTop: 16,
     marginBottom: 16,
     marginLeft: 36,
-    marginRight: 36
+    marginRight: 36,
   },
   input: {
-    fontSize: 18,
+    fontSize: 16,
     borderWidth: 1,
     padding: 12,
-    width: '80%',
-    borderRadius: 10,
-    backgroundColor: 'white',
+    width: "80%",
+    borderRadius: 15,
+    backgroundColor: "white",
   },
   image: {
     width: 120,
     height: 120,
-    borderColor: 'orange',
+    borderColor: "orange",
     borderWidth: 2,
     borderRadius: 100,
   },
   button: {
-    fontSize: 20,
-    color: 'white',
+    backgroundColor: "#ff385c",
+    padding: 12,
+    borderRadius: 15,
+  },
+  buttonText: {
     width: 120,
-    marginTop: 8,
     borderRadius: 10,
-    backgroundColor: '#c01c00',
     padding: 8,
-    textAlign: 'center'
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
   },
   message: {
     fontSize: 16,
     marginTop: 16,
     marginBottom: 16,
     marginLeft: 36,
-    marginRight: 36
+    marginRight: 36,
   },
   item: {
     backgroundColor: "lightblue",
@@ -238,6 +277,6 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
     borderRadius: 10,
-    alignItems: 'center'
+    alignItems: "center",
   },
 });
