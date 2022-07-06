51b31feb765e15a1c358b09d7ecbce36045b2cc7
garysum88 <102538356+garysum88@users.noreply.github.com>
2022-07-06 10:53:45 +0100

    Add ReviewView page to show all reviews

--- Pages/SecondaryPages/ReviewView.js
+++ Pages/SecondaryPages/ReviewView.js
@@ -1,10 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { getTimeDate } from '../../Utils/dayjs'
 
-export default function ReviewView() {
+export default function ReviewView(info) {
+  const locName = info.route.params.name
+  const reviews = info.route.params.reviews
+
   return (
     <View>
-      <Text>ReviewView</Text>
+      <Text>{ locName }</Text>
+      <Text></Text>
+      <View>{ reviews.map((review)=>{
+        return(
+        <View>
+          <Text> { review.ReviewBody}</Text>
+          <Text> { review.StarRating}</Text>
+          <Text> { review.VisitDate}</Text>
+          <Text> { getTimeDate(review.CreatedAt)}</Text>
+        </View>
+        )
+      }) }</View>
+
+
     </View>
   )
 }
